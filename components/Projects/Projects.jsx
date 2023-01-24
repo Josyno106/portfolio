2192e5fb8a9c01d9e54c05ba5fef6af47650f4bf
Dark Coder <tapamconsultancy@gmail.com>
2023-01-24 17:40:20 +0300

    changed the projects data array

--- components/Projects/Projects.jsx
+++ components/Projects/Projects.jsx
@@ -12,12 +12,67 @@ import { VscArrowSmallRight } from "react-icons/vsc";
 import thumbnail from "../../assets/thumbnail.png";
 import Link from "next/link";
 import SingleProject from "./SingleProject";
-import { ProjectsData } from "./Data";
+// import { ProjectsData } from "./Data";
 const Projects = () => {
   const iconsList = [
     <FaReact className="text-3xl animate-customspin text-blue-400" />,
     <SiFirebase className="text-3xl text-yellow-300  " />,
     <SiTailwindcss className="text-3xl text-blue-400  " />,
+  ];
+
+  const ProjectsData = [
+    {
+      title: "Fullstack Chat Web App",
+      iconList: [
+        <FaReact className="text-3xl animate-customspin text-blue-400" />,
+        <SiFirebase className="text-3xl text-yellow-300  " />,
+        <SiTailwindcss className="text-3xl text-blue-400  " />,
+      ],
+
+      description: `Chate is a fullstack real-time web chat app. I combined the power of Firebase Firestore real-time 
+      capabilities to bring the app to life.`,
+      thumbnail: thumbnail,
+      link: "https://github.com/Josyno106",
+    },
+    {
+      title: "Fullstack Chat Web App",
+      iconList: [
+        <FaReact className="text-3xl animate-customspin text-blue-400" />,
+        <SiFirebase className="text-3xl text-yellow-300  " />,
+        <SiTailwindcss className="text-3xl text-blue-400  " />,
+      ],
+
+      description: `Chate is a fullstack real-time web chat app. I combined the power of Firebase Firestore real-time 
+      capabilities to bring the app to life.`,
+      thumbnail: thumbnail,
+      link: "https://github.com/Josyno106",
+    },
+    {
+      title: "Fullstack Chat Web App",
+      iconList: [
+        <FaReact className="text-3xl animate-customspin text-blue-400" />,
+        <SiFirebase className="text-3xl text-yellow-300  " />,
+        <SiTailwindcss className="text-3xl text-blue-400  " />,
+      ],
+
+      description: `Chate is a fullstack real-time web chat app. I combined the power of Firebase Firestore real-time 
+      capabilities to bring the app to life.`,
+      thumbnail: thumbnail,
+      link: "https://github.com/Josyno106",
+    },
+    {
+      title: "Fullstack Chat Web App",
+      iconList: [
+        <FaReact className="text-3xl animate-customspin text-blue-400" />,
+        <SiFirebase className="text-3xl text-yellow-300  " />,
+        <SiTailwindcss className="text-3xl text-blue-400  " />,
+      ],
+
+      description: `Chate is a fullstack real-time web chat app. I combined the power of Firebase Firestore real-time 
+      capabilities to bring the app to life.`,
+      thumbnail: thumbnail,
+      link: "https://github.com/Josyno106",
+    },
   ];
 
   return (
