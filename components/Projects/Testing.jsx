cc8d02670a1cff93a07b5425b06a652db2395eff
Dark Coder <tapamconsultancy@gmail.com>
2023-02-01 20:48:27 +0300

    added mapping on items array in testing.jsx

--- components/Projects/Testing.jsx
+++ components/Projects/Testing.jsx
@@ -2,7 +2,11 @@ const Testing = () => {
   const array = ["Joshua", "Wanjugu", "Mary", "Lovely", "Focusly"];
   return (
     <>
-      <div></div>
+      <div>
+        {array.map((item, index) => {
+          <p key={`${index}+${item}`}>{item}</p>;
+        })}
+      </div>
     </>
   );
 };
