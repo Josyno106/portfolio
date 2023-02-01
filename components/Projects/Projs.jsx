22477f926db3394cc6acff5986b2b65c303b5eda
Dark Coder <tapamconsultancy@gmail.com>
2023-02-01 20:03:13 +0300

    commented projects

--- components/Projects/Projs.jsx
+++ components/Projects/Projs.jsx
@@ -93,19 +93,17 @@ const Projs = () => {
       </p>
 
       <div className="mt-16 mx-8 xl:mx-[12rem]  grid xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 gap-6 lg:mx-[6rem] md:mx-[5rem]">
-        {ProjectsData.map((project, index) => (
-          // <SingleProject
-          //   key={project.id}
-          //   title={project.title}
-          //   iconList={project.iconList}
-          //   description={project.description}
-          //   thumbnail={project.thumbnail}
-          //   to={project.link}
-          // />
-          <div key={`${project.id}+${index}`}>
-            <p>Test</p>{" "}
-          </div>
-        ))}
+        {/* {ProjectsData.map((project, index) => (
+          <SingleProject
+            key={project.id}
+            title={project.title}
+            iconList={project.iconList}
+            description={project.description}
+            thumbnail={project.thumbnail}
+            to={project.link}
+          />
+        
+        ))} */}
       </div>
     </div>
   );
